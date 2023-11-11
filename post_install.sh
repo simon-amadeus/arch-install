a29a9840ae1d6b2eef1743697f30893c36f51d2b
Simon Amadeus <simon.joedicke@code.berlin>
2023-11-11 22:49:39 +0100

    refactor: improve post install script

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Install Yay
 pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
@@ -17,15 +18,53 @@ makepkg -si
 cd ..
 rm -rf 1password
 
-# Install 1password-cli
-ARCH="amd64" && \
-    wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.20.0/op_linux_${ARCH}_v2.20.0.zip" -O op.zip && \
-    unzip -d op op.zip && \
-    sudo mv op/op /usr/local/bin && \
-    rm -r op.zip op && \
-    sudo groupadd -f onepassword-cli && \
-    sudo chgrp onepassword-cli /usr/local/bin/op && \
+
+# Install 1Password CLI
+echo "Installing 1Password CLI..."
+
+# Hardcoded version for initial install
+ARCH="amd64"
+VERSION="2.22.0"
+
+wget "https://cache.agilebits.com/dist/1P/op2/pkg/v$VERSION/op_linux_${ARCH}_v$VERSION.zip" -O op.zip
+unzip -d op op.zip
+sudo mv op/op /usr/local/bin
+rm -r op.zip op
+sudo groupadd -f onepassword-cli
+sudo chgrp onepassword-cli /usr/local/bin/op
+sudo chmod g+s /usr/local/bin/op
+
+echo "1Password CLI installed successfully."
+
+# Update 1Password CLI to the latest version
+echo "Checking for updates and installing if available..."
+UPDATE_DIR="$HOME/Downloads"
+mkdir -p "$UPDATE_DIR"
+cd "$UPDATE_DIR"
+
+# Use op update to download the latest version
+/usr/local/bin/op update --directory "$UPDATE_DIR"
+
+# Find the downloaded zip file
+ZIP_FILE=$(ls op_linux_${ARCH}_v*.zip | sort -V | tail -n 1)
+
+# If a new version was downloaded, install it
+if [ -n "$ZIP_FILE" ]; then
+    echo "New version found: $ZIP_FILE"
+    unzip -d op "$ZIP_FILE"
+    sudo mv op/op /usr/local/bin
+    rm -r "$ZIP_FILE" op
+
+    # Reapply group settings and permissions
+    sudo groupadd -f onepassword-cli
+    sudo chgrp onepassword-cli /usr/local/bin/op
     sudo chmod g+s /usr/local/bin/op
+
+    echo "1Password CLI updated successfully."
+else
+    echo "1Password CLI is already the latest version."
+fi
+
 
 # Install 1password hetzner cloud plugin
 sudo pacman -S hcloud
